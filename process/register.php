1d23aee55a6dd81e7a69d3cd174fbb2a40688667
josArt17 <josart1296@gmail.com>
2024-01-07 20:02:02 -0600

    mas cosas

---
+++ process/register.php
@@ -0,0 +1,46 @@
+<?php
+include ('conexion/conexion.php');
+
+function validate_user($correo){
+    global $conn;
+
+    $query = $conn->prepare("
+        SELECT usuario_id FROM control_usuario WHERE usuario_correo = :correo;
+    ");
+    $query->execute(array(
+        ":correo" => $correo
+    ));
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
+
+function register_user($nombre, $correo, $password){
+    global $conn;
+
+    $id = uniqid('ART_');
+
+    $query = $conn->prepare("
+        INSERT INTO control_usuario VALUES(:id, :nombre, :correo, :password)
+    ");
+    $query->execute(array(
+        ":id" => $id,
+        ":nombre" => $nombre,
+        ":correo" => $correo,
+        "password" => $password
+    ));
+}
+
+if (isset($_POST['nombre'])) {
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $password = $_POST['password'];
+
+    $consulta = validate_user($correo);
+
+    if ($consulta == false) {
+        register_user($nombre, $correo, $password);
+    }
+
+    header('Location: https://arthecnology.com/control/login.php');
+}
+?>
--- register.php
+++ register.php
@@ -7,30 +7,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Iniciar Sesion</title>
+    <title>Registrate</title>
 </head>
 <style>
     
 </style>
 <body>
 <main class="form-signin w-50 m-auto">
-  <form  action="process/login.php" method="POST">
+  <form  action="process/register.php" method="POST">
     <img class="mb-4" src="img/JA dev-logos_transparent.png" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Iniciar sesion</h1>
+    <h1 class="h3 mb-3 fw-normal">Registrarte</h1>
+
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="correo@email.com">
+      <input type="text" class="form-control" id="floatingInput" placeholder="nombre" name="nombre">
+      <label for="floatingInput">Nombre Completo</label>
+    </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="correo@email.com" name="correo">
       <label for="floatingInput">Correo electronico</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="********">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="********" name="password">
       <label for="floatingPassword">Contraseña</label>
     </div>
 
-    <div class="form-check text-start my-3">
-      <span>No tienes cuenta? <a href="register.php">Registrate aqui</a></span>
-    </div>
-    <button class="btn btn-primary w-100 py-2" type="submit">Iniciar sesion</button>
+    <button class="btn btn-primary w-100 py-2" type="submit">Registrarme</button>
     <p class="mt-5 mb-3 text-body-secondary">Arthecnology &copy; 2024–2030</p>
   </form>
 </main>
